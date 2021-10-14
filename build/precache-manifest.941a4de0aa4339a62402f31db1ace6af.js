55c9d7e713034348e69e259c0d4acb84d30255bd
Thinh Tran <thinhtran1769@gmail.com>
2021-10-13 20:01:04 -0400

    build folder added

---
+++ build/precache-manifest.941a4de0aa4339a62402f31db1ace6af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b08855255eb0553e65",
+    "url": "/thinhtrn/comp3104/static/css/main.fa1f0acd.chunk.css"
+  },
+  {
+    "revision": "33b08855255eb0553e65",
+    "url": "/thinhtrn/comp3104/static/js/main.33b08855.chunk.js"
+  },
+  {
+    "revision": "f45f88f1fc8cb775c9f7",
+    "url": "/thinhtrn/comp3104/static/js/runtime~main.f45f88f1.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/thinhtrn/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "0ec2093b03537fd62f5d06006331fa11",
+    "url": "/thinhtrn/comp3104/index.html"
+  }
+];
